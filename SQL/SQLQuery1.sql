8ef4a6be216faf892a8e98894311f9b5c19c2d58
Thomas Llewellyn <llewellyn.speck@gmail.com>
2019-04-30 17:01:29 -0500

    SQL intro

---
+++ SQL/SQLQuery1.sql
@@ -0,0 +1,33 @@
+-- comments
+-- when we dont want to run the whole file, we highlight what we want to run
+
+-- SQL has many commands/statements
+-- 
+Select firstname, CountryRegion
+from SalesLT.Customer, saleslt.Address
+where CountryRegion != 'United States'
+
+SELECT Distinct SalesLT.Address.CountryRegion
+FROM SalesLT.SalesOrderDetail, SalesLT.Address
+Where SalesLT.SalesOrderDetail.SalesOrderID != '0'
+--USing SalesLT.SalesOrderheader.ShipToAddressID
+
+
+SELECT count (distinct SalesLT.SalesOrderDetail.SalesOrderID), Sum(TotalDue)
+From SalesLT.SalesOrderDetail, SalesLT.SalesOrderHeader
+--where SalesLT.SalesOrderHeader.OrderDate
+--Between '2009-01-01 00:00:00.000' AND '2009-12-31 00:00:00.000'
+
+Select Count(SalesOrderID), Sum(TotalDue), CountryRegion
+From SalesLT.SalesOrderHeader, SalesLT.Address
+Group by CountryRegion
+Order By Count(TotalDue) DESC
+
+
+
+Select *
+From SalesLT.SalesOrderHeader
+--Order by SalesOrderID Desc
+
+Select TotalDue
+from SalesLT.SalesOrderHeader
