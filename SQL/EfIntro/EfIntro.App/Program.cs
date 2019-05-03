65970e48b165c73ea1aa0350b0eed2615ca7f930
Thomas Llewellyn <llewellyn.speck@gmail.com>
2019-05-03 13:38:13 -0500

    database and c# practice

---
+++ SQL/EfIntro/EfIntro.App/Program.cs
@@ -0,0 +1,63 @@
+﻿using EfIntro.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EfIntro.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                 using (MovieDbContext dbContext = CreateDbContext())
+            {
+                PrintMovies(dbContext);
+
+                AddAMovie(dbContext);
+
+                PrintMovies(dbContext);
+
+                UpdateAMovie(dbContext);
+
+                PrintMovies(dbContext);
+
+                DeleteAMovie(dbContext);
+
+                PrintMovies(dbContext);
+
+            }
+
+
+        }
+
+        private static void DeleteAMovie(MovieDbContext dbContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void UpdateAMovie(MovieDbContext dbContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AddAMovie(MovieDbContext dbContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PrintMovies(MovieDbContext dbContext)
+        {
+            foreach (var movie in dbContext.Movie)
+            {
+                Console.WriteLine($"{movie.MovieId}: {movie.Title} ({movie.ReleaseDate.Year})");
+            }
+            Console.WriteLine();
+        }
+
+        private static MovieDbContext CreateDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MovieDbContext>();
+            optionsBuilder.UseSqlServer(SecretConfiguration.ConnectionString);
+            return new MovieDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ SQL/EfIntro/EfIntro.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EfIntro.DataAccess
+{
+    public class Class1
+    {
+    }
+}
