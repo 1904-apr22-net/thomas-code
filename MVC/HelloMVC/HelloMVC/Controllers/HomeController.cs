8449d927e886fd690657eac0f91c57344d65799d
Thomas Llewellyn <llewellyn.speck@gmail.com>
2019-05-08 15:56:06 -0500

    mvc practice

---
+++ MVC/HelloMVC/HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HelloMVC.Models;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // controllers have "action methods"
+        // each action method is meant to handle a request to one path
+        // it should return a view result to be rendered into HTML and sent as a HTTP response.
+
+            // this index method handles requests to the following paths:
+            // - /
+            // - /home
+            // - /Home/Index
+        public IActionResult Index()
+        {
+            //so this means get the home/Index view and return it
+            // home 
+
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            //get the privacy view and return it
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC/HelloMVC/HelloMVC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace HelloMVC
+{
+    public class Program
+    {
+        //entry point for ASP.NET
+        public static void Main(string[] args)
+        {
+            //Creates web hose and runs it
+            CreateWebHostBuilder(args).Build().Run();
+        }
+        // here we have a lot of default configuration
+        // we do plug in logging in this file
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
