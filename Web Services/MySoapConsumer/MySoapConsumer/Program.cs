8b16bc548fcdc528272e34dc9e2ced0f8f0f8a3a
Thomas Llewellyn <llewellyn.speck@gmail.com>
2019-05-21 11:37:43 -0500

    web services introduction

---
+++ Web Services/MySoapConsumer/MySoapConsumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using MySoapConsumer.UnitConversionService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySoapConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new UnitConversionServiceClient())
+            {
+
+            }
+        }
+    }
+}
---
+++ Web Services/MySoapService/MySoapService/IUnitConversionService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MySoapService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IUnitConversionService" in both code and config file together.
+    [ServiceContract] // means, this interface will be a service having some operations.
+    public interface IUnitConversionService
+    {
+        [OperationContract] // means, this method will be accessible as a SOAP operation
+        //[OperationBehavior] // can configure transaction semantics, etc
+        double FeetToMeters(double feet);
+
+        [OperationContract]
+        Temperature ConvertTemperature(Temperature temperature);
+    }
+}
